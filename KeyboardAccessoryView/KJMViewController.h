cc83f53148f5cc53853e77e93e0898d2ec25d21a
Kyle <k@kylejm.com>
2013-12-13 01:02:53 +0000

    First commit

--- KeyboardAccessoryView/KJMViewController.h
+++ KeyboardAccessoryView/KJMViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface KJMViewController : UIViewController
+@interface KJMViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- KeyboardAccessoryView/KJMViewController.m
+++ KeyboardAccessoryView/KJMViewController.m
@@ -10,6 +10,9 @@
 
 @interface KJMViewController ()
 
+@property (strong, nonatomic) UITextField *textField1;
+@property (strong, nonatomic) UITextField *textField2;
+
 @end
 
 @implementation KJMViewController
@@ -17,13 +20,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.textField1 = [[UITextField alloc]initWithFrame:CGRectMake(30, 7, 260, 30)];
+    self.textField1.borderStyle = UITextBorderStyleRoundedRect;
+    self.textField1.delegate = self;
+    UIToolbar *navToolbar = self.navigationController.toolbar;
+    [navToolbar addSubview:self.textField1];
+    
+    UIToolbar *toolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 44)];
+    self.textField2 = [[UITextField alloc]initWithFrame:CGRectMake(30, 7, 260, 30)];
+    self.textField2.borderStyle = UITextBorderStyleRoundedRect;
+    self.textField2.delegate = self;
+    [toolbar addSubview:self.textField2];
+    self.textField1.inputAccessoryView = toolbar;
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(firstRes:) name:UIKeyboardDidShowNotification object:nil];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)firstRes:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.textField2 becomeFirstResponder];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if (textField == self.textField2) {
+        self.textField1.text = self.textField2.text;
+    }
+    [textField resignFirstResponder];
+    [self.textField1 resignFirstResponder];
+    return YES;
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter]removeObserver:self forKeyPath:UIKeyboardDidShowNotification];
+    [super viewDidDisappear:animated];
 }
 
 @end
